9db0261eeec6a01ac6d38367590355bf39221912
Sergey Zalunin <z.a.l.u.n.i.n.s.e.r.g@gmail.com>
2020-07-26 07:26:49 +0600

    created boids map that allows boids to know position of each other

--- boid.go
+++ boid.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math"
 	"math/rand"
 	"time"
 )
@@ -12,20 +13,48 @@ type Boid struct {
 	id       int
 }
 
+func (b *Boid) calcAcceleration() Vector2D {
+	upper, lower := b.position.AddV(viewRadius), b.position.AddV(-viewRadius)
+	avgVelocity := Vector2D{0, 0}
+	count := 0.0
+
+	for i := math.Max(lower.x, 0); i <= math.Min(upper.x, screenWidth); i++ {
+		for j := math.Max(lower.y, 0); j <= math.Min(upper.y, screenHeight); j++ {
+			if otherBoidID := boidMap[int(i)][int(j)]; otherBoidID != -1 && otherBoidID != b.id {
+				if dist := boids[otherBoidID].position.Distance(b.position); dist <= viewRadius {
+					count++
+					avgVelocity = avgVelocity.Add(boids[otherBoidID].velocity)
+				}
+			}
+		}
+	}
+
+	accel := Vector2D{0, 0}
+	if count > 0 {
+		avgVelocity = avgVelocity.DivisionV(count)
+		accel = avgVelocity.Subtract(b.velocity).MultiplyV(adjRate)
+	}
+	return accel
+}
+
 func (b *Boid) moveOne() {
+	b.velocity = b.velocity.Add(b.calcAcceleration()).Limit(-1, 1)
+	boidMap[int(b.position.x)][int(b.position.y)] = -1
 	b.position = b.position.Add(b.velocity)
+	boidMap[int(b.position.x)][int(b.position.y)] = b.id
+
 	next := b.position.Add(b.velocity)
 
 	if next.x >= screenWidth || next.x < 0 {
-		b.velocity = Vector2D{ -b.velocity.x, b.velocity.y }
+		b.velocity = Vector2D{-b.velocity.x, b.velocity.y}
 	}
 
-	if next.y >= screenWidth || next.y < 0 {
-		b.velocity = Vector2D{ b.velocity.x, -b.velocity.y }
+	if next.y >= screenHeight || next.y < 0 {
+		b.velocity = Vector2D{b.velocity.x, -b.velocity.y}
 	}
 }
 
-func (b *Boid) start()  {
+func (b *Boid) start() {
 	for {
 		b.moveOne()
 		time.Sleep(5 * time.Millisecond)
@@ -33,14 +62,13 @@ func (b *Boid) start()  {
 }
 
 func createBoid(bid int) {
-//	rand.Seed(time.Now().UnixNano())
-
 	b := Boid{
 		position: Vector2D{rand.Float64() * screenWidth, rand.Float64() * screenHeight},
-		velocity: Vector2D{rand.Float64()*2 - 1.0, rand.Float64()*2 + 1.0},
+		velocity: Vector2D{rand.Float64() * 2 - 1.0, rand.Float64() * 2 + 1.0},
 		id:       bid,
 	}
 
 	boids[bid] = &b
+	boidMap[int(b.position.x)][int(b.position.y)] = b.id
 	go b.start()
 }
--- main.go
+++ main.go
@@ -10,11 +10,14 @@ import (
 const (
 	screenWidth, screenHeight = 640, 360
 	boidCount                 = 500
+	viewRadius                = 13
+	adjRate                   = 0.015
 )
 
 var (
 	green = color.RGBA{10, 255, 50, 255}
 	boids [boidCount]*Boid
+	boidMap [screenWidth + 1][screenHeight + 1]int
 )
 
 func update(screen *ebiten.Image) error {
@@ -30,6 +33,12 @@ func update(screen *ebiten.Image) error {
 }
 
 func main() {
+	for i, row := range boidMap {
+		for j := range row {
+			boidMap[i][j] = -1
+		}
+	}
+
 	for i := 0; i < boidCount; i++ {
 		createBoid(i)
 	}
